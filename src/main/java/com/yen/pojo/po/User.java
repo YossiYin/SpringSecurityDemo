b73d2cdc27df43cd62b4e07f2ca71c2450d34608
Horace <youseeyhx@gmail.com>
2024-04-19 16:50:41 +0800

    更新实体类

--- src/main/java/com/yen/pojo/po/User.java
+++ src/main/java/com/yen/pojo/po/User.java
@@ -6,7 +6,11 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 
@@ -14,6 +18,9 @@ import lombok.Data;
  */
 @TableName(value ="sys_user")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements Serializable {
     /**
      * 主键Id
